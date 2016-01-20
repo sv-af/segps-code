e597d45971a5884951d52c34625bf1a1fb35b292
Techy Geek <to_sultan@yahoo.com>
2016-01-20 12:22:07 -0500

    Updates SecurityDBsABox and added NVDTriplesPublisher class

---
+++ ca.concordia.cs.aseg.segps.ontologies.publisher/src/ca/concordia/cs/aseg/segps/ontologies/publisher/raw_data_reader/NVDtriples.java
@@ -0,0 +1,16 @@
+/*
+ * Created by ASEG at Concordia University.
+ * http://aseg.cs.concordia.ca
+ * http://aseg.cs.concordia.ca/segps
+ * Please see the LICENSE file for details.
+ */
+package ca.concordia.cs.aseg.segps.ontologies.publisher.raw_data_reader;
+
+public class NVDtriples {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- ca.concordia.cs.aseg.segps.ontologies.urigenerator/src/ca/concordia/cs/aseg/segps/ontologies/urigenerator/domain_specific/abox/SecurityDBsABox.java
+++ ca.concordia.cs.aseg.segps.ontologies.urigenerator/src/ca/concordia/cs/aseg/segps/ontologies/urigenerator/domain_specific/abox/SecurityDBsABox.java
@@ -7,6 +7,20 @@
 
 package ca.concordia.cs.aseg.segps.ontologies.urigenerator.domain_specific.abox;
 
+import ca.concordia.cs.aseg.segps.ontologies.urigenerator.namespace.NamespaceFactory;
+import ca.concordia.cs.aseg.segps.ontologies.urigenerator.registry.NamespaceRegistry;
+import ca.concordia.cs.aseg.segps.ontologies.urigenerator.registry.OntologyRegistry;
+
 public class SecurityDBsABox {
+	public static String Vulnerability(String Vulnerability) {
+		String uri = NamespaceFactory.createAboxNamespace(NamespaceRegistry.theAboxNameSpace, OntologyRegistry.securityDBs)
+				+ Vulnerability;
+		return uri;
+	}
+	
+//	public static void main(String[] args) {
+//		String vulnerability = Vulnerability("CVE-2013-2020");
+//		System.out.println(vulnerability);
+//	}
 
 }
