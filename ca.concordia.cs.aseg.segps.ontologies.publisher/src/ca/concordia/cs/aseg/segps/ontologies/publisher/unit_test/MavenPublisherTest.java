c354639ce7973d420b71ba8e0a0d015cce6278ad
elliseghan <elliseghan@gmail.com>
2016-01-25 15:48:53 -0500

    Added Maven publisher code

---
+++ ca.concordia.cs.aseg.segps.ontologies.publisher/src/ca/concordia/cs/aseg/segps/ontologies/publisher/unit_test/MavenPublisherTest.java
@@ -0,0 +1,23 @@
+package ca.concordia.cs.aseg.segps.ontologies.publisher.unit_test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import ca.concordia.cs.aseg.segps.ontologies.publisher.build.MavenPublisher;
+
+public class MavenPublisherTest {
+
+	static File testFile = new File("D:\\eclipse\\workspace\\maven-processor\\src\\test\\resources\\testpom.pom");
+	static File outFile = new File("sample-triples.nt");
+	@Test
+	public void test() {
+		MavenPublisher publisher = new MavenPublisher();
+		publisher.publish(outFile.getAbsolutePath(), testFile.getAbsolutePath());
+		long fileSize = outFile.length();
+		assertNotEquals(0, fileSize);
+	}
+
+}
