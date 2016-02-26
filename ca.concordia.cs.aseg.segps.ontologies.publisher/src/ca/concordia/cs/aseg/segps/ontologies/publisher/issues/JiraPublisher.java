0f0115238e4d670ed61d0fed050b1b6db6dc1745
umroot <umroot@concept12.encs.concordia.ca>
2016-02-26 15:28:10 -0500

    New: added RDF reader
    Next: map rdf data to the issue ontology

---
+++ ca.concordia.cs.aseg.segps.ontologies.publisher/src/ca/concordia/cs/aseg/segps/ontologies/publisher/issues/JiraPublisher.java
@@ -0,0 +1,9 @@
+package ca.concordia.cs.aseg.segps.ontologies.publisher.issues;
+
+public class JiraPublisher {
+
+	/**
+	 * publishing java projects issues hosted in jira issue tracker.
+	 */
+	
+}
---
+++ ca.concordia.cs.aseg.segps.ontologies.publisher/src/ca/concordia/cs/aseg/segps/ontologies/publisher/issues/ReadingRDFdata.java
@@ -0,0 +1,78 @@
+package ca.concordia.cs.aseg.segps.ontologies.publisher.issues;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintStream;
+
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+
+
+public class ReadingRDFdata {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		
+	/*	Model model = FileManager.get().loadModel("raw_data/tika.rdf");
+		
+		StmtIterator iter;
+		Statement stmt;
+
+		Property predicate;
+		Resource subject;
+		RDFNode obj;
+		
+		iter = model.listStatements();
+		while (iter.hasNext()) {
+			stmt = iter.next();
+			subject = stmt.getSubject();
+			System.out.println("Subject = " + subject.getURI());
+			predicate = stmt.getPredicate();
+			System.out.println("Predicate = " +predicate.getLocalName());
+			obj = stmt.getObject();
+			System.out.println("Object = " + obj.toString());
+		}*/
+		
+		File inputFolder = new File("raw_data/");
+		String outputFolder = "output/";
+		
+		String query = "extract rdf data"; 
+		try{
+			PrintStream console = System.out;
+			for (final File fileEntry : inputFolder.listFiles()) {
+				PrintStream out = new PrintStream(new FileOutputStream(outputFolder+
+						fileEntry.getName() + ".csv"));
+				OntModel model = ModelFactory.createOntologyModel(OntModelSpec.OWL_DL_MEM);
+				System.out.println("Parsing "+fileEntry.toString() );
+				System.setOut(out);
+				File file = new File(fileEntry.toString());
+				FileReader reader = new FileReader(file);
+				model.read(reader, null);
+
+				Query query = QueryFactory.create(bugQueryt);
+				QueryExecution qe = QueryExecutionFactory.create(query, model);
+
+				ResultSet resutls = qe.execSelect();
+				ResultSetFormatter.outputAsCSV(out, resutls);
+				// return the activation to the system.out
+				System.setOut(console);
+				System.out.println("Done !!");
+				
+			}
+			
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
