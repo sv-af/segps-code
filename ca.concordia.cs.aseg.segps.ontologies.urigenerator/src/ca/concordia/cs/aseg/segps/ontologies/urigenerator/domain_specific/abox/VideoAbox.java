ae466f4c76afc66ffd9dd8b6acc759a8d79405b3
elliseghan <elliseghan@gmail.com>
2017-12-05 12:15:14 -0500

    Added more methods to Video ABox and TBox classes

--- ca.concordia.cs.aseg.segps.ontologies.urigenerator/src/ca/concordia/cs/aseg/segps/ontologies/urigenerator/domain_specific/abox/VideoAbox.java
+++ ca.concordia.cs.aseg.segps.ontologies.urigenerator/src/ca/concordia/cs/aseg/segps/ontologies/urigenerator/domain_specific/abox/VideoAbox.java
@@ -3,16 +3,45 @@ package ca.concordia.cs.aseg.segps.ontologies.urigenerator.domain_specific.abox;
 import ca.concordia.cs.aseg.segps.ontologies.urigenerator.namespace.NamespaceFactory;
 import ca.concordia.cs.aseg.segps.ontologies.urigenerator.registry.NamespaceRegistry;
 import ca.concordia.cs.aseg.segps.ontologies.urigenerator.registry.OntologyRegistry;
+import ca.concordia.cs.aseg.segps.ontologies.urigenerator.utils.URLCleaner;
 
 public class VideoAbox {
-	public static String Video(String url){
-		String uri= url;
+	
+	public static String Video(String videoURL) {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theAboxNameSpace, OntologyRegistry.videos)
+				+ URLCleaner.clean(release);
+		return uri;
+	}
+
+	public static String Speech(String video, String speechID) {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theAboxNameSpace, OntologyRegistry.videos)
+				+ URLCleaner.clean(release);
+		return uri;
+	}
+
+	public static String Image(String video, String imageID) {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theAboxNameSpace, OntologyRegistry.videos)
+				+ URLCleaner.clean(release);
+		return uri;
+	}
+
+	public static String Topic(String video, String topicName) {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theAboxNameSpace, OntologyRegistry.videos)
+				+ URLCleaner.clean(release);
+		return uri;
+	}
+
+	public static String Comment(String video, String commentID, String commenter) {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theAboxNameSpace, OntologyRegistry.videos)
+				+ URLCleaner.clean(release);
 		return uri;
 	}
 	
-	public static String Author(String name){
-		String uri= NamespaceFactory.createAboxNamespace(NamespaceRegistry.theAboxNameSpace, OntologyRegistry.videos) + name;
+	public static String Publisher(String publisherName) {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theAboxNameSpace, OntologyRegistry.videos)
+				+ URLCleaner.clean(publisherName);
 		return uri;
 	}
-	
+
+
 }
--- ca.concordia.cs.aseg.segps.ontologies.urigenerator/src/ca/concordia/cs/aseg/segps/ontologies/urigenerator/domain_specific/tbox/VideoTBox.java
+++ ca.concordia.cs.aseg.segps.ontologies.urigenerator/src/ca/concordia/cs/aseg/segps/ontologies/urigenerator/domain_specific/tbox/VideoTBox.java
@@ -11,96 +11,120 @@ public class VideoTBox {
 	/*
 	 * CONCEPTS
 	 */
-	public static String Video(){
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "Video";
+	public static String Video() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "Video";
+		return uri;
+	}
+
+	public static String Speech() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "Speech";
+		return uri;
+	}
+
+	public static String Image() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "Image";
+		return uri;
+	}
+
+	public static String Topic() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "Topic";
+		return uri;
+	}
+
+	public static String Comment() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "Comment";
 		return uri;
 	}
 	
-	public static String Speech(){
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "Speech";
+	public static String Publisher() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "Publisher";
 		return uri;
 	}
-		
-	public static String Image(){
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "Image";
-		return uri;
-	}
-	
-	public static String Topic(){
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "Topic";
-		return uri;
-	}
-	
-	public static String Comment(){
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "Comment";
-		return uri;
-	}
+
 	/*
-	 * PROPERTIES
+	 * OBJECT PROPERTIES
 	 */
-	public static String hasSpeech(){
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "hasSpeech";
+	public static String hasSpeech() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "hasSpeech";
 		return uri;
 	}
-	
-	public static String hasImage(){
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "hasImage";
+
+	public static String hasImage() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "hasImage";
 		return uri;
 	}
-	
-	public static String hasTopic(){
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "hasTopic";
+
+	public static String hasTopic() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "hasTopic";
 		return uri;
 	}
-	
-	public static String hasComment(){
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "hasComment";
-		return uri;
-	}
-	
-	public static String hasText(){
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "hasText";
-		return uri;
-	}
-	
-	public static String hasKeyword(){
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "hasKeyword";
+
+	public static String hasComment() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "hasComment";
 		return uri;
 	}
 	
 	public static String hasReply() {
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "hasReply";
-		return uri;
-	}
-	
-	public static String isReplyOf() {
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "isReplyOf";
-		return uri;
-	}
-	
-	public static String hasLabel() {
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "hasLabel";
-		return uri;
-	}
-	
-	public static String hasTitle() {
-		String uri= NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos) + "hasTitle";
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "hasReply";
 		return uri;
 	}
 
+	public static String isReplyOf() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "isReplyOf";
+		return uri;
+	}
 	
+	/*
+	 * DATA PROPERTIES
+	 */
+	public static String hasText() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "hasText";
+		return uri;
+	}
+
+	public static String hasKeyword() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "hasKeyword";
+		return uri;
+	}
+
+	public static String hasLabel() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "hasLabel";
+		return uri;
+	}
+
+	public static String hasTitle() {
+		String uri = NamespaceFactory.createTboxNamespace(NamespaceRegistry.theTboxNameSpace, OntologyRegistry.videos)
+				+ "hasTitle";
+		return uri;
+	}
+
 	public static void main(String[] args) {
-		
+
 		String video, author, title, url;
-		url= "https://www.youtube.com/watch?v=EO91xsIPd5o";
-		author=VideoAbox.Author("Radio Javan");
-		video=VideoAbox.Video(url);
-		title="Title1";
-		
-		System.out.println(video +"\t\t"+RDF.type()+"\t\t"+VideoTBox.Video());
-		System.out.println(author +"\t\t"+RDF.type()+"\t\t"+MainTBox.Stakeholder());
-		System.out.println(video +"\t\t"+MainTBox.hasAuthor()+"\t\t"+author);
-//		System.out.println(VideoTBox.Speech());
-//		System.out.println(VideoTBox.Image());
+		url = "https://www.youtube.com/watch?v=EO91xsIPd5o";
+		author = VideoAbox.Author("Radio Javan");
+		video = VideoAbox.Video(url);
+		title = "Title1";
+
+		System.out.println(video + "\t\t" + RDF.type() + "\t\t" + VideoTBox.Video());
+		System.out.println(author + "\t\t" + RDF.type() + "\t\t" + MainTBox.Stakeholder());
+		System.out.println(video + "\t\t" + MainTBox.hasAuthor() + "\t\t" + author);
+		// System.out.println(VideoTBox.Speech());
+		// System.out.println(VideoTBox.Image());
 	}
 }
